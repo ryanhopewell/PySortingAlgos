585925b9a4491321a3a00e3352554a4801120328
ryan hopewell <ryanahopewell@outlook.com>
2024-11-18 19:08:44 +0000

    add bubble sort algo

---
+++ py_sort_algorithms.py
@@ -0,0 +1,16 @@
+my_array = [64, 34, 25, 12, 22, 11, 90, 5]
+
+def bubble_sort(arr):
+
+    n = len(arr)
+
+    for i in range(n-1):
+
+        for j in range (n-i-1):
+
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    print(f"Sorted Array: {arr}")
+
+bubble_sort(my_array)
