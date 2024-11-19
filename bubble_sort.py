bd59dfaca1d2d8a1e45da52b7229b887b19f5a70
ryan hopewell <ryanahopewell@outlook.com>
2024-11-19 14:11:49 +0000

    add improved bubble sort function

--- bubble_sort.py
+++ bubble_sort.py
@@ -13,4 +13,28 @@ def bubble_sort(arr):
 
     print(f"Sorted Array: {arr}")
 
+def improved_bubble_sort(arr):
+
+    n = len(arr)
+
+    for i in range(n-1):
+
+        swapped = False
+
+        for j in range (n-i-1):
+
+            if arr[j] > arr[j+1]:
+                
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = False
+
+        
+        if not swapped:
+
+            break
+
+    print(f"Sorted Array: {arr}")
+
 bubble_sort(my_array)
+improved_bubble_sort(my_array)
+
